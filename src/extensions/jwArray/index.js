290eb3f7531dfd59a0a4426866197215fbec68db
arc360alt <voidyt16@gmail.com>
2026-02-23 15:48:16 -0600

    Revert "remake isObject"
    This reverts commit 4e53faff00061a1cbc59ac15ec143d855baf5d16.
    
    revert

--- src/extensions/jwArray/index.js
+++ src/extensions/jwArray/index.js
@@ -44,7 +44,16 @@ function span(text) {
 }
 
 function isObject(x) {
-    return x !== null && typeof x === "object" && [null, Object.prototype].includes(Object.getPrototypeOf(x));
+    const fnToString = Function.prototype.toString
+    const classRegex = /^class\s/
+    if (typeof x === "function") {
+        return !classRegex.test(fnToString.call(x))
+    }
+    if (x !== null && typeof x === "object") {
+        const ctor = x.constructor
+        return !(typeof ctor === "function" && classRegex.test(fnToString.call(ctor)))
+    }
+    return false
 }
 
 class ArrayType {
